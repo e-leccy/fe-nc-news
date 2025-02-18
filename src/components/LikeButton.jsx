4f39c4d0ea0be7efc9a7779e2d5236f450caa07a
e-leccy <eleccy@aim.com>
2025-02-18 15:37:55 +0000

    refactor LikeButton to be DRY

--- src/components/LikeButton.jsx
+++ src/components/LikeButton.jsx
@@ -7,24 +7,20 @@ function LikeButton ({article}) {
     const [upButtonDisabled, setUpButtonDisabled] = useState(false)
     const [downButtonDisabled, setDownButtonDisabled] = useState(false)
 
-    function upVote() {
-        setVote((currentVote) => currentVote +1)
-        updateArticle(article.article_id, 1)
-        setUpButtonDisabled(true)
-        
+    function handleVote(voteChange){
+        setVote((currentVote) => currentVote + voteChange)
+        updateArticle(article.article_id, voteChange)
+        if (voteChange === 1){
+            setUpButtonDisabled(true)
+        } else {
+            setDownButtonDisabled(true)
+        }
     }
 
-    function downVote() {
-        setVote((currentVote) => currentVote -1)
-        updateArticle(article.article_id, -1)
-        setDownButtonDisabled(true)
-    }
-
-    
     return (<>
     <p>Votes:{vote} </p>
-    <button onClick={upVote} disabled={upButtonDisabled}>Yay</button>
-    <button onClick={downVote} disabled={downButtonDisabled}>Nay</button>
+    <button onClick={() => handleVote(1)} disabled={upButtonDisabled}>Yay</button>
+    <button onClick={() => handleVote(-1)} disabled={downButtonDisabled}>Nay</button>
 
     </>)
     
