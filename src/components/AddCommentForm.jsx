db21147ad8d36bedd210dbb2e9abc19cb2c2be7c
e-leccy <eleccy@aim.com>
2025-02-20 14:44:50 +0000

    forms cannot be empty

--- src/components/AddCommentForm.jsx
+++ src/components/AddCommentForm.jsx
@@ -1,59 +1,66 @@
-import { useState } from "react"
-import { postComment } from "../utils/api"
-import CommentCard from "./CommentCard"
-import { UserAccount } from "./UserAccount"
-import { useContext } from "react"
+import { useState } from "react";
+import { postComment } from "../utils/api";
+import CommentCard from "./CommentCard";
+import { UserAccount } from "./UserAccount";
+import { useContext } from "react";
 
-function AddCommentForm({article_id, setCommentsUpdate}) {
-    const {loggedInUser, setLoggedInUser} = useContext(UserAccount)
-    const [commentToAdd, setCommentToAdd] = useState({username: loggedInUser, body: ""})
-    const [errorMsg, setErrorMsg] = useState(false)
-    const [posting, setPosting] = useState(false)
+function AddCommentForm({ article_id, setCommentsUpdate }) {
+  const { loggedInUser, setLoggedInUser } = useContext(UserAccount);
+  const [commentToAdd, setCommentToAdd] = useState({
+    username: loggedInUser,
+    body: "",
+  });
+  const [errorMsg, setErrorMsg] = useState(false);
+  const [posting, setPosting] = useState(false);
 
-    function handleChange(event) {
-    const name = event.target.name 
-    const value = event.target.value
-    setCommentToAdd(values => ({...values, [name]: value}))
-    }
+  function handleChange(event) {
+    const name = event.target.name;
+    const value = event.target.value;
+    setCommentToAdd((values) => ({ ...values, [name]: value }));
+  }
 
-    function handleSubmit(event){
-    event.preventDefault()
-    setCommentsUpdate(false)
-    setPosting(true)
+  function handleSubmit(event) {
+    event.preventDefault();
+    setErrorMsg(false);
+    setCommentsUpdate(false);
+    setPosting(true);
     postComment(article_id, commentToAdd)
-    .then((response) => {
-        <CommentCard comment={commentToAdd} />
-        setCommentsUpdate(true)
-        setPosting(false)
-        setCommentToAdd({body: ""})
-        setErrorMsg(false)
-    })
-    .catch((error) => {
-        setPosting(false)
-        setErrorMsg(true)
-        console.log(error)
-    })
-    }
+      .then((response) => {
+        <CommentCard comment={commentToAdd} />;
+        setCommentsUpdate(true);
+        setPosting(false);
+        setCommentToAdd({ body: "" });
+        setErrorMsg(false);
+      })
+      .catch((error) => {
+        setPosting(false);
+        setErrorMsg(true);
+        console.log(error);
+      });
+  }
 
-    if (posting){
-        return <p>Posting...</p>
-    }
+  if (posting) {
+    return <p>Posting...</p>;
+  }
 
-return (
+  return (
     <>
-<form>
-    
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="comment"
+          className="comment-box"
+          name="body"
+          required
+          value={commentToAdd.body}
+          onChange={handleChange}
+        />
 
-    
-    <input type="text" placeholder='comment' className="comment-box" name="body" required
-    value={commentToAdd.body} onChange={handleChange} />
-
-    <button type="submit" onClick={handleSubmit} >Submit</button>
-
-</form>
-{errorMsg ? (<p>Something went wrong!</p>) : null}
-</>
-)
+        <button type="submit">Submit</button>
+      </form>
+      {errorMsg ? <p>Something went wrong!</p> : null}
+    </>
+  );
 }
 
-export default AddCommentForm
+export default AddCommentForm;
--- src/components/UserLoginForm.jsx
+++ src/components/UserLoginForm.jsx
@@ -1,56 +1,61 @@
 import { useContext, useState } from "react";
 import { UserAccount } from "./UserAccount";
 
-function UserLoginForm ({users}) {
+function UserLoginForm({ users }) {
+  const [selectedUser, setSelectedUser] = useState({ username: "" });
+  const { loggedInUser, setLoggedInUser } = useContext(UserAccount);
+  const [searching, setSearching] = useState(false);
+  const [errorMsg, setErrorMsg] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(false);
 
-    const [selectedUser, setSelectedUser] = useState({username: ""})
-    const { loggedInUser, setLoggedInUser} = useContext(UserAccount)
-    const [searching, setSearching] = useState(false)
-    const [errorMsg, setErrorMsg] = useState(false)
-    const [loggedIn, setLoggedIn] = useState(false)
+  function handleChange(event) {
+    const value = event.target.value;
+    setSelectedUser({ username: value });
+  }
 
-    function handleChange(event) {
-        const value = event.target.value
-        setSelectedUser({username: value })
-        
+  function handleSubmit(event) {
+    event.preventDefault();
+    setSearching(true);
+    const userExist = users.some(
+      (user) => user.username === selectedUser.username
+    );
+
+    if (userExist) {
+      setLoggedInUser(selectedUser.username);
+      setSearching(false);
+      setLoggedIn(true);
+      setErrorMsg(false);
+    } else {
+      setSearching(false);
+      setErrorMsg(true);
     }
+  }
 
-    function handleSubmit(event){
-        event.preventDefault()
-        setSearching(true)
-        const userExist = users.some(user => user.username === selectedUser.username)
-        
-        
-        if (userExist) {
-            setLoggedInUser(selectedUser.username)            
-            setSearching(false)
-            setLoggedIn(true)
-            setErrorMsg(false)
-            
-        } else {
-            setSearching(false)
-            setErrorMsg(true)
-        }
-    }
+  if (searching) {
+    return <p>Searching...</p>;
+  }
 
-    if (searching) {
-        return <p>Searching...</p>
-    }
-
-return (
-<>
-    <form>
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="username">Username:</label>
-        <input type="text" placeholder='username' id="username" name="username" required
-        value={selectedUser.username} onChange={handleChange} />
+        <input
+          type="text"
+          placeholder="username"
+          id="username"
+          name="username"
+          required
+          value={selectedUser.username}
+          onChange={handleChange}
+        />
 
-    <button type="submit" onClick={handleSubmit} >Submit</button>
-    </form>
+        <button type="submit">Submit</button>
+      </form>
 
-    {errorMsg ? (<p>Something went wrong!</p>) : null}
-    {loggedIn ? (<p>Logged in as {selectedUser.username}</p>) : null}
-</>
-)
+      {errorMsg ? <p>Something went wrong!</p> : null}
+      {loggedIn ? <p>Logged in as {selectedUser.username}</p> : null}
+    </>
+  );
 }
 
-export default UserLoginForm
+export default UserLoginForm;
