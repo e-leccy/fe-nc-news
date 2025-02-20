4f3d8766c523da7840828c7d5b048ed874efdb7f
e-leccy <eleccy@aim.com>
2025-02-20 11:31:11 +0000

    add aditional way to login

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,14 +1,23 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { UserAccount } from "./UserAccount";
 
-function UserCard({user}) {
- return (
+function UserCard({ user }) {
+  const { loggedInUser, setLoggedInUser } = useContext(UserAccount);
+  function handleClick() {
+    if (!loggedInUser) {
+      setLoggedInUser(user.username);
+    }
+  }
+
+  return (
     <div className="user">
-    <Link to={`/users/${user.username}`}>
-    <h2>{user.username}</h2>
-    </Link>
-    <img src={user.avatar_url} alt="avatar image"/>
+      <Link to={`/users/${user.username}`} onClick={handleClick}>
+        <h2>{user.username}</h2>
+      </Link>
+      <img src={user.avatar_url} alt="avatar image" />
     </div>
- )
+  );
 }
 
-export default UserCard
+export default UserCard;
